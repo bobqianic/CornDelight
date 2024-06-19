00f67d86b197ffaf5b6d2c0d186741cef2006991
KreloX <krelox@seznam.cz>
2024-06-19 14:03:27 +0200

    feat: make corn compostable
    Made certain corn items compostable.

--- src/main/java/krelox/corndelight/CornDelight.java
+++ src/main/java/krelox/corndelight/CornDelight.java
@@ -6,6 +6,7 @@ import krelox.corndelight.item.CornDelightItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.ComposterBlock;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -20,9 +21,18 @@ public class CornDelight implements ModInitializer {
         CornDelightItems.registerItems();
         CornDelightBlocks.registerBlocks();
 
-        ItemGroupEvents.modifyEntriesEvent(FarmersDelightMod.ITEM_GROUP).register(entries -> CornDelightItems.items.forEach(entries::add));
+        ItemGroupEvents.modifyEntriesEvent(FarmersDelightMod.ITEM_GROUP).register(entries -> CornDelightItems.ITEMS.forEach(entries::add));
+
+        registerCompostables();
 
         BiomeModifications.addFeature(context -> context.getBiome().getTemperature() > 0f && context.getBiome().getTemperature() <= 1f,
                 GenerationStep.Feature.VEGETAL_DECORATION, RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MODID, "patch_wild_corn")));
+    }
+
+    public static void registerCompostables() {
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(CornDelightItems.CORN_SEEDS, 0.3F);
+
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(CornDelightItems.CORN, 0.65F);
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(CornDelightBlocks.WILD_CORN.asItem(), 0.65F);
     }
 }
--- src/main/java/krelox/corndelight/data/CornDelightLang.java
+++ src/main/java/krelox/corndelight/data/CornDelightLang.java
@@ -15,7 +15,7 @@ public class CornDelightLang extends FabricLanguageProvider {
     public void generateTranslations(TranslationBuilder translationBuilder) {
         translationBuilder.add(CornDelightBlocks.CORN_CROP, "Corn");
         translationBuilder.add("corndelight.block.popcorn.barehand", "Use your bare hand to eat popcorn.");
-        CornDelightItems.items.forEach(item -> translationBuilder.add(item, formatName(item.toString())));
+        CornDelightItems.ITEMS.forEach(item -> translationBuilder.add(item, formatName(item.toString())));
     }
 
     @SuppressWarnings("deprecation")
